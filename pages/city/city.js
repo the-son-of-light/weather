27af7d9afe78276862143a58bdb702eb8dd3b787
unknown <136844692@qq.com>
2019-05-15 09:46:03 +0800

    多个页面

--- pages/city/city.js
+++ pages/city/city.js
@@ -5,9 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+	searchCtiy:'',
+	searchValue:'',
+	hotCity:['北京','上海','广州','深圳','天津','杭州','东莞','宁波','西安','成都','重庆','南京','苏州','武汉','厦门','福州','昆明','沈阳','长春',
+			'大连','济南','青岛','郑州','兰州','太原','合肥','哈尔滨','长沙','石家庄','南昌','珠海','香港','澳门','台北']
   },
 
+//   获取输入的城市
+	getCity:function(e){
+		let searchValue = e.detail.value;
+		let chinese =/^[\u4e00-\u9fa5]+$/;
+		if(!chinese.test(searchValue) && searchValue != ''){
+			wx.showModal({
+				title: '提示',
+				content: '请输入汉字！',
+				success: function (res) {
+					if (res.confirm) {
+						console.log('用户点击确定')
+					}else{
+					   console.log('用户点击取消')
+					}
+				}
+			})
+			this.setData({
+				searchValue:''
+      		})
+		}else{
+			this.setData({
+				searchValue:searchValue
+			})
+		}
+	},
+  chooseCity:function(){
+    let cityName = this.data.searchValue;
+    getApp().globalData.showDialog  = cityName;
+		wx.switchTab({
+      url: '../index/index',
+      success: function(e) {
+        var page = getCurrentPages().pop();
+        if (page == undefined || page == null) return;
+       		page.onLoad();
+      	}
+	})
+		console.log(getApp().globalData.showDialog)
+    console.log(this.data.searchValue)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,10 @@
 // 通过百度地图获取天气
 let bmap = require('../../libs/bmap-wx.js');
-let utils = require('../../utils/util')
+let utils = require('../../utils/util');
+
 Page({
 	data: {
+		allData:[],
 		weatherData: '',
 		currentCity:'',
 		currentData:'',
@@ -20,65 +22,93 @@ Page({
 		'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557805325705&di=924564518b71744c63875aad0ecb8fa9&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F01%2F43%2F85%2F59399ce26e45f_610.jpg',
 		'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557805360804&di=d8f70e629f09b15f7e205193ac24409d&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F02%2F40%2F04%2F59c2d4fb8593b_610.jpg'
 		]
-  },
-  onLoad: function () {
-    let TIME = utils.formatTime(new Date());
-    let that = this;
-    // 新建百度地图对象 
-    let BMap = new bmap.BMapWX({
-      ak: 'VilotzCpuWGy7XGOeU0P6iKbb2YotcPY'
-    });
-    let fail = function (data) {
-      console.log(data)
-    };
-    let success = function (data) {
-		let weatherData = data.currentWeather[0];
-		let weatherDesc = data.originalData;
-		let tempPic = weatherDesc.results[0];
-		let tempImg = tempPic.weather_data;
-		if(TIME > "18:00"){
-			that.setData({
-			tempPic:tempImg[0].nightPictureUrl
-			})
-		}else{
-			that.setData({
-			tempPic:tempImg[0].dayPictureUrl
-			})
-		}
-		let fingerData =  tempPic.index;
-		for(let i in that.data.fingerPic){
-			fingerData[i].name = that.data.fingerPic[i]
-		}
-		
-		for(let i = 0;i < tempImg.length;i ++){
-			if(TIME > "18:00"){
-				that.data.pic.push(tempImg[i].nightPictureUrl)
-			}else{
-				that.data.pic.push(tempImg[i].dayPictureUrl)
-			}
-		}
-		for(let i in that.data.pic){
-			tempImg[i].nowPic = that.data.pic[i]
-			console.log(tempImg[i])
-		}
-		tempImg[0].date = tempImg[0].date.substring(0,3)
-		that.setData({
-			weatherData: weatherData,
-			currentCity:weatherData.currentCity,
-			currentData:weatherDesc.date,
-			weather:weatherData.weatherDesc,
-			temperature:weatherData.temperature,
-			nowTemperture:weatherData.date.substring(14,17),
-			finger:fingerData,
-			weatherDes:tempImg
-		});
-		console.log(that.data.weatherDes)
-		
-    }
-    // 发起weather请求 
-    BMap.weather({
-      fail: fail,
+	},
+	onShareAppMessage: function() {
+    let users = wx.getStorageSync('user');
+    if (data.from === 'button') {}
+    return {
+      title: '转发',
+      path: '/pages/index/index',
       success: success
-    });
-  }
+    }
+	},
+  	onLoad: function () {
+		let city = getApp().globalData.showDialog;
+		this.setData({
+			currentCity:city
+		})
+		console.log(city)
+    	let TIME = utils.formatTime(new Date());
+    	let that = this;
+    	// 新建百度地图对象 
+    	let BMap = new bmap.BMapWX({
+     		 ak: 'VilotzCpuWGy7XGOeU0P6iKbb2YotcPY'
+    	});
+		let fail = function (data) {
+		console.log(data)
+		};
+		let success = function (data) {
+				let allData = that.data.allData;
+				allData.push(data);
+				that.setData({
+					allData:allData
+				})
+				console.log(allData.length)
+				console.log(that.data.allData)
+				for(let i = 0;i < allData.length;i ++){
+					let weatherData = allData[i].currentWeather[i].currentCity;
+					let currentData = allData[i].originalData.date;
+					that.setData({
+						currentCity:weatherData,
+						currentData:currentData
+					})
+					console.log(weatherData)
+				}
+
+			let weatherData = data.currentWeather[0];
+			let weatherDesc = data.originalData;
+			let tempPic = weatherDesc.results[0];
+			let tempImg = tempPic.weather_data;
+			if(TIME > "18:00"){
+				that.setData({
+				tempPic:tempImg[0].nightPictureUrl
+				})
+			}else{
+				that.setData({
+				tempPic:tempImg[0].dayPictureUrl
+				})
+			}
+			let fingerData =  tempPic.index;
+			for(let i in that.data.fingerPic){
+				fingerData[i].name = that.data.fingerPic[i]
+			}
+			for(let i = 0;i < tempImg.length;i ++){
+				if(TIME > "18:00"){
+					that.data.pic.push(tempImg[i].nightPictureUrl)
+				}else{
+					that.data.pic.push(tempImg[i].dayPictureUrl)
+				}
+			}
+			for(let i in that.data.pic){
+				tempImg[i].nowPic = that.data.pic[i]
+			}
+			tempImg[0].date = tempImg[0].date.substring(0,3)
+			that.setData({
+				weatherData: weatherData,
+				currentCity:weatherData.currentCity,
+				currentData:weatherDesc.date,
+				weather:weatherData.weatherDesc,
+				temperature:weatherData.temperature,
+				nowTemperture:weatherData.date.substring(14,17),
+				finger:fingerData,
+				weatherDes:tempImg
+			});
+		// console.log(that.data.allData)	
+    	}
+		// 发起weather请求 
+		BMap.weather({
+		fail: fail,
+		success: success
+		});
+  	}
 })
